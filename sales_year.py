a4854b18e6e4ca356b15b2f93c5f99e838a49d58
marquesa96 <62727827+marquesa96@users.noreply.github.com>
2021-12-04 10:52:36 -0600

    Add files via upload

---
+++ sales_year.py
@@ -0,0 +1,9 @@
+from lifestore_file import lifestore_products, lifestore_sales, lifestore_searches
+"""
+lifestore_products = [0 id_product, 1 name, 2 price, 3 category, 4 stock]
+lifestore_sales = [id_sale, id_product, score (from 1 to 5), date, refund (1 for true or 0 to false)]
+lifestore_searches = [id_search, id product]
+""" 
+
+
+print ()
